e529f3457d828105a8c09f96b1c71e3c9a53066a
Jessé Azevêdo <jessedanobrega@gmail.com>
2024-01-14 17:14:27 -0300

    first commit

---
+++ src/components/PaginaPadrao/index.tsx
@@ -0,0 +1,18 @@
+import styles from './PaginaPadrao.module.scss';
+import { Outlet } from 'react-router-dom';
+import stylesTema from 'styles/Tema.module.scss';
+
+export default function PaginaPadrao() {
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.header__text}>
+              A Casa do Código e da Massa
+        </div>
+      </header>
+      <div className={stylesTema.container}>
+        <Outlet />
+      </div> 
+    </>
+  );
+}
